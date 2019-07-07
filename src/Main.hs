a16ad5228a3a0b62788fe84684813788cf2a16cb
Anton Gushcha <ncrashed@gmail.com>
2019-07-07 08:02:52 +0300

    Add classifier feature from pom file

--- src/Main.hs
+++ src/Main.hs
@@ -29,6 +29,8 @@ import System.IO.Temp
 import System.Process
 import Text.XML.Light
 
+import Debug.Trace
+
 data HavenEnv = HavenEnv
   { _havenEnv_manager :: Manager
   , _havenEnv_repos :: Map String String
@@ -61,12 +63,16 @@ main = do
       e <- liftIO $ hIsEOF hTmpFile
       unless e $ do
         line <- liftIO $ hGetLine hTmpFile
+        traceM line
         let mavenGrArTyVr = dropWhile (not . isAlphaNum) line -- Skip leading symbols; we don't care about parsing this
             (groupId, ':':mavenArTyVr) = break (==':') mavenGrArTyVr
             (artifactId, ':':mavenTyVr) = break (==':') mavenArTyVr
-            (fileType, ':':mavenVr) = break (==':') mavenTyVr -- File type that Maven has decided on
-            version = takeWhile (/=':') mavenVr -- Version number
-            maven = Maven groupId artifactId version
+            (fileType, ':':mavenClVr) = break (==':') mavenTyVr -- File type that Maven has decided on
+            (clOrVr, ':':mver) = break (==':') mavenClVr
+            (classifier, version) = if takeWhile (/=':') mver == mver
+              then (Nothing, clOrVr)
+              else (Just clOrVr, takeWhile (/=':') mver)
+            maven = Maven groupId artifactId version classifier
         unless (all isSpace line) $ do
           mMvnNix <- runReaderT (runMaybeT $ fetch fileType maven) havenEnv
           case mMvnNix of
@@ -91,12 +97,14 @@ data Maven = Maven
   { _maven_groupId :: String
   , _maven_artifactId :: String
   , _maven_version :: String
+  , _maven_classifier :: Maybe String
   }
   deriving (Show, Read, Eq, Ord)
 
 data MavenNix = MavenNix
   { _mavenNix_maven :: Maven
   , _mavenNix_repo :: String
+  , _mavenNix_classifier :: Maybe String
   , _mavenNix_jarSha256 :: Maybe (Digest SHA256State)
   , _mavenNix_pomSha256 :: Maybe (Digest SHA256State)
   , _mavenNix_aarSha256 :: Maybe (Digest SHA256State)
@@ -108,7 +116,7 @@ toNix :: MavenNix -> String
 toNix m =
   let mvn = _mavenNix_maven m
       showHash h = fromMaybe "null" $ (\x -> "\"" <> x <> "\"") . showDigest <$> h
-  in unlines
+  in unlines $
       [ "  { artifactId = \"" <> _maven_artifactId mvn <> "\";"
       , "    groupId = \"" <> _maven_groupId mvn <> "\";"
       , "    version = \"" <> _maven_version mvn <> "\";"
@@ -116,7 +124,9 @@ toNix m =
       , "    jarSha256 = " <> showHash (_mavenNix_jarSha256 m) <> ";"
       , "    pomSha256 = " <> showHash (_mavenNix_pomSha256 m) <> ";"
       , "    aarSha256 = " <> showHash (_mavenNix_aarSha256 m) <> "; }"
-      ]
+      ] ++ maybe [] (\v -> [
+        "    classifier = \"" <> v <> "\";"
+      ]) (_maven_classifier mvn)
 
 -- | Gets the repo with the given id, calling 'empty' when it's not present
 getRepo :: (MonadReader HavenEnv m, MonadPlus m) => String -> m String
@@ -128,8 +138,9 @@ m2Directory :: Maven -> String
 m2Directory mvn = foldl (</>) ""
   [ (\x -> if x == '.' then '/' else x) <$> _maven_groupId mvn
   , _maven_artifactId mvn
-  , _maven_version mvn
+  , _maven_version mvn -- ++ maybe "" ("-" <>) (_maven_classifier mvn)
   ]
+
 
 -- | Gets a given artifact for a 'Maven' and hashes it. It will first
 -- check the local m2 dir, and then it will try to download it from
@@ -145,7 +156,8 @@ getArtifactFile mvn ext repo = do
   mgr <- asks _havenEnv_manager
   m2Repo <- asks _havenEnv_m2Local
   let m2Dir = m2Directory mvn
-      m2Filename = _maven_artifactId mvn <> "-" <> _maven_version mvn <> ext
+      classPostfix = if ext == ".pom" then "" else maybe "" ("-" <>) (_maven_classifier mvn)
+      m2Filename = _maven_artifactId mvn <> "-" <> _maven_version mvn <> classPostfix <> ext
       path = m2Repo </> m2Dir </> m2Filename
   m2ArtifactExists <- liftIO $ doesFileExist path
   if m2ArtifactExists then liftIO (BL.readFile path) else do
@@ -173,10 +185,11 @@ fetch fileType mvn = do
   repo <- getRepo repoId
 
   pom <- runMaybeT $ getArtifactFile mvn ".pom" repo
-  
+
   let noArtifacts = MavenNix
         { _mavenNix_maven = mvn
         , _mavenNix_repo = repo
+        , _mavenNix_classifier = _maven_classifier mvn
         , _mavenNix_jarSha256 = Nothing
         , _mavenNix_pomSha256 = sha256 <$> pom
         , _mavenNix_aarSha256 = Nothing
@@ -189,7 +202,8 @@ fetch fileType mvn = do
       groupId <- strContent <$> findChildrenByTagName "groupId" parent
       artifactId <- strContent <$> findChildrenByTagName "artifactId" parent
       version <- strContent <$> findChildrenByTagName "version" parent
-      return $ Maven groupId artifactId version
+      let classifier = headMay $ strContent <$> findChildrenByTagName "classifier" parent
+      return $ Maven groupId artifactId version classifier
 
   -- TODO: Match the 'type' to the correct file extension.
   -- The extension is _usually_ equal to the type, but it's not necessarily.
@@ -208,6 +222,10 @@ fetch fileType mvn = do
       return noArtifacts
     ]
   return (mavenNix:parents)
+
+headMay :: [a] -> Maybe a
+headMay [] = Nothing
+headMay (x:_) = Just x
 
 -- | Retrieve an XML Element's children by tag name
 findChildrenByTagName :: String -> Element -> [Element]
